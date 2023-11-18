1a7ca3c3b47e3d910593ba43a72e44696414ced2
zaidkamil <xaidmetamorphos@gmail.com>
2023-11-18 18:15:10 +0530

    first commit

---
+++ core/settings.py
@@ -0,0 +1,73 @@
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = 'django-insecure-(l#%qa6n-w6vgy)q536186us5pt1fcvkk2h4avww7c03v!8z06'
+DEBUG = True
+ALLOWED_HOSTS = []
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    "django_htmx",
+    'home',
+    'accounts',
+]
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     "django_htmx.middleware.HtmxMiddleware",
+]
+ROOT_URLCONF = 'core.urls'
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR.joinpath('templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+WSGI_APPLICATION = 'core.wsgi.application'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'Asia/Kolkata'
+USE_I18N = True
+USE_TZ = True
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR.joinpath('staticfiles')]
+STATIC_ROOT = BASE_DIR.joinpath('static')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR.joinpath('media')
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ readme.md
@@ -0,0 +1,55 @@
+# Project Description
+
+This project is a Django application setup with htmx and Tailwind CSS. It provides a starting point for building dynamic web applications with Django, htmx, and Tailwind CSS.
+
+## Features
+
+- Django framework for backend development
+- htmx for seamless and dynamic frontend interactions
+- Tailwind CSS for responsive and modern UI design
+
+## Installation
+
+1. Clone the repository:
+
+    ```bash
+    git clone <repository_url>
+    ```
+
+2. Change into the project directory:
+
+    ```bash
+    cd digipodium-django-htmx-tailwind-setup
+    ```
+
+3. Install the project dependencies:
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+4. Run database migrations:
+
+    ```bash
+    python manage.py migrate
+    ```
+
+5. Start the development server:
+
+    ```bash
+    python manage.py runserver
+    ```
+
+6. Open your web browser and visit `http://localhost:8000` to see the application in action.
+
+## Usage
+
+- Use the Django admin interface to manage the backend data.
+- Explore the project structure and customize it according to your needs.
+- Leverage htmx and Tailwind CSS to build dynamic and responsive frontend components.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request on the GitHub repository.
+
+
